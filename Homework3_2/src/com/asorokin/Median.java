e0089da2a15cb9867627d5c3d198bce1393d1070
A_SoRocking <a.sorocking@gmail.com>
2018-03-06 20:25:47 +0300

    Corrected remarks from your comments

--- Homework3_2/src/com/asorokin/Median.java
+++ Homework3_2/src/com/asorokin/Median.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.asorokin;
 
-/**
- *
- * @author sorokin
- */
-public class Median {
+import java.util.Arrays;
+
+public final class Median {
+    //method to find median of array of integer numbers
+    public static float median(int[] array) {
+        Arrays.sort(array);          
+        int center = array.length / 2; 
+        if (array.length%2 == 1) {   //check even or odd array lenght
+            return array[center];    
+        } else {
+            //even array lenght, cast result of division to float 
+            return (float)(array[center-1] + array[center]) / 2;
+        }
+    }
     
+    //method to find median of array of double numbers
+    public static double median(double[] array) {
+        Arrays.sort(array);          
+        int center = array.length / 2; 
+        if (array.length%2 == 1) {
+            return array[center];
+        } else {
+            return (array[center-1] + array[center]) / 2.0;
+        }
+    }
 }
+
+
