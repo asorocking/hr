63ef1e44d58dac85a9a262298b08fd5f4394d7fa
A_SoRocking <a.sorocking@gmail.com>
2018-03-13 15:47:20 +0300

    some working1

--- Homework5/src/com/asorokin/Homework5.java
+++ Homework5/src/com/asorokin/Homework5.java
@@ -1,146 +1,13 @@
 package com.asorokin;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * "Component"
- */
-interface Path {
-
-    String name = "";
-    String extension = "";
-    String parent = "";
-
-    //Prints the path.
-    public void print();
-
-}
-
-/**
- * "Composite"
- */
-class Folder implements Path {
-    
-    String name = "";
-    String extension = "";
-    String parent = "";
-    
-    //Collection of child paths.
-    private List<Path> mChildPaths = new ArrayList<Path>();
-    
-    public Folder (String name, String extension, String parent){
-        this.name = name;
-        this.extension = extension;
-        this.parent = parent;
-    }
-    
-    //Prints the path.
-    public void print() {
-        for (Path path : mChildPaths) {
-            path.print();
-        }
-    }
-
-    //Adds the path to the composition.
-    public void add(Path path) {
-        mChildPaths.add(path);
-    }
-
-    //Removes the path from the composition.
-    public void remove(Path path) {
-        mChildPaths.remove(path);
-    }
-
-}
-
-/**
- * "Leaf"
- */
-class File implements Path {
-
-    String name = "";
-    String extension = "";
-    String parent = "";
-
-    public File() {
-    }
-
-    public File(String name, String extension) {
-        this.name = name;
-        this.extension = extension;
-    }
-
-    //Prints the path.
-    public void print() {
-        System.out.println(name + "." + extension);
-    }
-
-}
-
-/**
- * Client
- */
-class Printer {
-    static String[] subStr;
-    static String delimeter = "/";;
-    
-    public static void print(String realPath) {
-        subStr  = realPath.split(delimeter);
-        // Вывод результата на экран
-        for (int i = 0; i< subStr.length; i++) {
-            System.out.println(subStr[i]);
-        }
-    }
-}
-
-class Parser {
-    public static List<File> mFiles = new ArrayList<File>();
-    public static void parsePath(String realPath) {
-        
-    }
-    static String[] subStr;
-    static String delimeter = "/";;
-    
-    public static void print(String realPath) {
-        int arrayIndex = mFiles.size();
-        subStr  = realPath.split(delimeter);
-        // Вывод результата на экран
-        for (int i = 0; i < subStr.length; i++) {
-            mFiles.add(arrayIndex, new File(subStr[subStr.length], subStr[subStr.length-1]));
-            System.out.println(subStr[i]);
-        }
-    }
-}
-
-public class Homework5 {
+class Homework5 {
 
     public static void main(String[] args) {
-        /* //Initialize four files
-        File file1 = new File("autoexec", "bat");
-        File file2 = new File("config", "sys");
-        File file3 = new File("client", "mdb");
-        File file4 = new File("cbmain", "ex");
-
-        //Initialize three composite paths
-        Folder path = new Folder();
-        Folder path1 = new Folder();
-        Folder path2 = new Folder();
-
-        //Composes the paths
-        path1.add(file1);
-        path1.add(file2);
-        path1.add(file3);
-
-        path2.add(file4);
-
-        path.add(path1);
-        path.add(path2);
-
-        //Prints the complete path (four times the string "File").
-        path.print();
-         */
+       
         String enterString, realPath = "";
         int command = 3;
 
@@ -148,11 +15,12 @@ public class Homework5 {
             Scanner sc = new Scanner(System.in);
             System.out.println("Введите путь или команды print или close:");
             enterString = sc.nextLine();
-            
+
             if (enterString.equals("print")) {
                 command = 1;
             }
             if (enterString.indexOf("/") != -1) {
+                realPath = enterString;
                 command = 2;
             }
 
@@ -166,7 +34,171 @@ public class Homework5 {
             }
         } while (!enterString.equals("close"));
 
-        boolean endOfPath = false;
+    }
+}
+
+//Client
+class Printer {
+
+    static String[] subStr;
+    static String delimeter = "/";
+
+    public static void print(String realPath) {
+        subStr = realPath.split(delimeter);
+        // Вывод результата на экран
+        for (int i = 0; i < subStr.length; i++) {
+            System.out.println(subStr[i]);
+        }
+    }
+}
+
+class Parser {
+
+    public static List<Component> mPath = new ArrayList<Component>();
+    static String[] subStr;
+    static String delimeter = "/";
+
+    public static void parsePath(String realPath) {
+        subStr = realPath.split(delimeter);
+        if (isLegalSymbol(subStr[0]) && !isExistElement(subStr[0], "FileSystem")) {
+            mPath.add(new Directory(subStr[0], "FileSystem", 0));
+        }        
+        for (int i = 1; i < subStr.length; i++) {
+            if (subStr[i].indexOf('.') == -1) {
+                if (isLegalSymbol(subStr[i]) && !isExistElement(subStr[i], subStr[i-1])) {
+                    mPath.add(new Directory(subStr[i], subStr[i-1], i));
+                }
+            } else {
+                if (isLegalSymbol(subStr[i]) && !isExistElement(subStr[i], subStr[i-1])) {
+                    mPath.add(new File(subStr[i], subStr[i-1], i));
+                }
+                break;
+            }
+        }
+
+        print(mPath);
+    }
+    
+    private static void print(List<Component> mPath) {
+        
+        for (int i = 0; i < mPath.size(); i++) 
+            {
+            System.out.println("Вывод из mPath " + mPath.get(i).getLevel() + ": " + mPath.get(i).getParent() + "/" + mPath.get(i).getName());
+        }
+    }
+    
+    private static boolean isExistElement(String name, String parent) {
+        boolean result = false;
+        
+        for (int i = 0; i < mPath.size(); i++) {
+            boolean parentExist = mPath.get(i).getParent().equals(parent);
+            boolean nameExist = mPath.get(i).getName().equals(name);
+            if (parentExist && nameExist) result = true;
+        }
+        return result;
+    }
+    
+    private static boolean isLegalSymbol(String str) {
+        return str.matches("^[a-zA-Z0-9-_.\\s]+$");
+    }
+}
+
+interface Component {
+
+    
+
+    public String getName();
+    
+    public String getParent();
+    
+    public int getLevel();
+
+    public void add(Component component);
+
+    public void remove(Component component);
+
+    public void print();
+}
+
+class Directory implements Component {
+
+    private List<Component> components = new ArrayList<Component>();
+    String name = "";
+    String parent = "";
+    int level = 0;
+
+    public Directory(String name, String parent, int level) {
+        this.name = name;
+        this.parent = parent;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getParent() {
+        return parent;
+    }
+    
+    public int getLevel(){
+        return level;
+    }
+    
+
+    @Override
+    public void add(Component component) {
+        components.add(component);
+    }
+
+    @Override
+    public void remove(Component component) {
+        components.remove(component);
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Узел " + name);
+        System.out.println("Подузлы:");
+        for (int i = 0; i < components.size(); i++) {
+            components.get(i).print();
+        }
+    }
+}
+
+class File implements Component {
+
+    String name = "";
+    String parent = "";
+    int level = 0;
+
+    public File(String name, String parent, int level) {
+        this.name = name;
+        this.parent = parent;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getParent() {
+        return parent;
+    }
+    public int getLevel(){
+        return level;
+    }
+    
+    public void add(Component component) {
 
     }
+
+    public void remove(Component component) {
+
+    }
+
+    public void print() {
+        System.out.println("Файл " + name);
+    }
+
 }
