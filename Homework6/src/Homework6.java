b6316f7880cd646b965e87def2e3bfd48b38ff3d
asorokin <a.sorocking@gmail.com>
2018-03-15 19:33:24 +0300

    t

--- Homework6/src/Homework6.java
+++ Homework6/src/Homework6.java
@@ -9,7 +9,7 @@ public class Homework6 {
 
     public static void main(String[] args) {
         System.out.println("Введите текст:");
-        //12As         I          love         animals, so I’ve got a lot of them. Someone’s hobby is collecting stamps, doing exercises, drawing or learning foreign languages. Animals are my hobby. I like to look after them, feed and just watch them. I believe that animals bring us happiness and positive emotions. This, in its turn, has a positive effect on our moral and psychological state.
+        //12As        I          love         animals, so I’ve got a lot of them. Someone’s hobby is collecting stamps, doing exercises, drawing or learning foreign languages. Animals are my hobby. I like to look after them, feed and just watch them. I believe that animals bring us happiness and positive emotions. This, in its turn, has a positive effect on our moral and psychological state.
        
         StringBuilder testString = new StringBuilder();
         Scanner sc = new Scanner(System.in);
