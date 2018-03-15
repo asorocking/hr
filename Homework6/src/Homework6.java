d19642c8a6fe5847e71c45a8caaa508cd9a22d0d
A_SoRocking <a.sorocking@gmail.com>
2018-03-15 11:21:46 +0300

    create Homework6

---
+++ Homework6/src/Homework6.java
@@ -0,0 +1,11 @@
+public class Homework6 {
+
+    public static void main(String[] args) {
+        StringBuilder testString = new StringBuilder("Once upon a time a Wolf "
+                + "was lapping at a spring on a hillside, when, looking up, "
+                + "what should he see but Lamb just beginning to drink "
+                + "a little lower down.");
+        
+    }
+    
+}
