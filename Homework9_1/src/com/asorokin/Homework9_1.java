2ec3e0e987e8ed5e49abfb8a306db6dcbef558d4
asorokin <a.sorocking@gmail.com>
2018-03-30 20:06:35 +0300

    Homework9_1 by Comparator

--- Homework9_1/src/com/asorokin/Homework9_1.java
+++ Homework9_1/src/com/asorokin/Homework9_1.java
@@ -6,7 +6,8 @@ import java.util.Comparator;
 public class Homework9_1 implements Comparator<Integer> {
 
     public static void main(String[] args) {
-        Integer[] massive = {25, 1627, -2147483646, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421};
+        Integer[] massive = {25, 1627, -2147483646, 9999, 2147483647, 753, 1,
+            0, 10, 5000, 5421};
 
         sort(massive);
 
@@ -14,21 +15,21 @@ public class Homework9_1 implements Comparator<Integer> {
             System.out.print(i + " ");
         }
     }
-    public static int multy(int a) {
-        return 10 + a;
-    }
+
     public static Integer[] sort(Integer[] massive) {
         Arrays.sort(massive, new Comparator<Integer>() {
             @Override
             public int compare(Integer a, Integer b) {
                 Integer summ1 = 0;
                 Integer summ2 = 0;
+
                 if (a < 0) {
                     a *= -1;
                 }
                 if (b < 0) {
                     b *= -1;
                 }
+
                 for (int d = 1000000000; d >= 1; d /= 10) {
                     if (a / d != 0) {
                         summ1 += a / d;
