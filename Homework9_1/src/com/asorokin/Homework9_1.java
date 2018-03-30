1f4e8293dffef8f8dfa0101089e0e2be8581d97e
A_SoRocking <a.sorocking@gmail.com>
2018-03-30 17:38:09 +0300

    1

--- Homework9_1/src/com/asorokin/Homework9_1.java
+++ Homework9_1/src/com/asorokin/Homework9_1.java
@@ -6,100 +6,45 @@ import java.util.Comparator;
 public class Homework9_1 implements Comparator<Integer> {
 
     public static void main(String[] args) {
-        Integer[] massive = {25, 1627, -2147483648, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421};
-        int[] temp = summDigits((int[]) massive);
-        //https://habrahabr.ru/post/329498/
-        for (int i = 0; i < temp.length; i++){
-            System.out.print(temp[i] + " ");
+        Integer[] massive = {25, 1627, -2147483646, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421};
+
+        sort(massive);
+
+        for (int i : massive) {
+            System.out.print(i + " ");
         }
-        
-        Integer[] summ = null;
-        for (int i = 0; i < temp.length; i++){
-            summ[i] = (Integer) temp[i];
-            System.out.print(temp[i] + " ");
-            System.out.print(summ[i] + " ");
-        }
-        Arrays.sort(summ, new Comparator<Integer>() {
+    }
+    public static int multy(int a) {
+        return 10 + a;
+    }
+    public static Integer[] sort(Integer[] massive) {
+        Arrays.sort(massive, new Comparator<Integer>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
+            public int compare(Integer a, Integer b) {
+                Integer summ1 = 0;
+                Integer summ2 = 0;
+                if (a < 0) {
+                    a *= -1;
+                }
+                if (b < 0) {
+                    b *= -1;
+                }
+                for (int d = 1000000000; d >= 1; d /= 10) {
+                    if (a / d != 0) {
+                        summ1 += a / d;
+                        a %= d;
+                    }
+                    if (b / d != 0) {
+                        summ2 += b / d;
+                        b %= d;
+                    }
+                }
+                return summ1.compareTo(summ2);
             }
         });
- 
-        for (int i : summ)
-            System.out.print(i + " ");
-        /*
-        Integer[] data = {25, 1627, -2147483648, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421};
-        
-        Arrays.sort(data, new Comparator<Integer>() {
-            public int compare(Integer o1, Integer o2) {
-                int summ1 = 0;
-                int summ2 = 0;
-                for (int d = 1000000000; d >= 1; d /= 10) {
-                    if (o1 / d != 0) {
-                        summ1 += o1 / d;
-                        o1 %= d;
-                    }
-                    if (o2 / d != 0) {
-                        summ2 += o2 / d;
-                        o2 %= d;
-                    }
-                }
-                if (summ1 < 0) {
-                    summ1 *= -1;
-                }
-                o1 = (Integer) summ1;
-                if (summ2 < 0) {
-                    summ2 *= -1;
-                }
-                o2 = (Integer) summ2;
-                return o1.compareTo(o2);
-            }
-
-            // return o1.compareTo(o2);
-        }
-        );
-        
-        System.out.println(Arrays.toString(data));
-        */
+        return massive;
     }
 
-    
-    public static int[] summDigits(int[] massive) {
-        int[] summ = new int[massive.length];
-
-        for (int i = 0; i < massive.length; i++) {
-            for (int d = 1000000000; d >= 1; d /= 10) {
-                if (massive[i] / d != 0) {
-                    summ[i] += massive[i] / d;
-                    massive[i] %= d;
-                }
-            }
-            if (summ[i] < 0) {
-                summ[i] *= -1;
-            }
-            System.out.print(summ[i] + ", ");
-        }
-        System.out.println();
-        return summ;
-    }
-    
-
- /*
-    public static int[] sort(int[] summ){
-        for (int i = 0; i < summ.length; i++) {
-            for (int j = i + 1; j < summ.length; j++) {
-                if (summ[i] > summ[j]) {
-                    int tmp = summ[i];
-                    summ[i] = summ[j];
-                    summ[j] = tmp;
-                }
-            }
-            System.out.print(summ[i] + ", ");
-        }
-        return summ;
-    }
-     */
     @Override
     public int compare(Integer a, Integer b) {
         return a.compareTo(b);
--- Homework9_1/test/com/asorokin/Homework9_1Test.java
+++ Homework9_1/test/com/asorokin/Homework9_1Test.java
@@ -4,52 +4,60 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 
-@RunWith(Parameterized.class)
+//@RunWith(Parameterized.class)
 public class Homework9_1Test extends Assert {
 
-    Homework9_1 hw9_1 = new Homework9_1();
-    private int[] testData;
-    private int[] expectedResult;
+    //Homework9_1 hw9_1 = new Homework9_1();
+    //private Integer[] testData;
+    //private Integer[] expectedResult;
 
-    public Homework9_1Test(int[] testData, int[] expectedResult) {
-        this.testData = testData;
-        this.expectedResult = expectedResult;
-    }
-
-    @Test
-    public void testSummDigits() {
-        int[] result = hw9_1.summDigits(testData);
-        assertTrue(Arrays.equals(expectedResult, result));
+    public Homework9_1Test(/*Integer[] testData, Integer[] expectedResult*/) {
+      //  this.testData = testData;
+      //  this.expectedResult = expectedResult;
     }
     
     @Test
+    public void multy() {
+        //Integer[] massiv = {11, 74, 125};
+        //Integer[] expectedResult = {11, 125, 74};
+        //Integer[] result = Homework9_1.sort(massiv);
+        int a = 1;
+        int expectedResult = 11;
+        int result = Homework9_1.multy(a);
+        //Assert.assertArrayEquals(expectedResult, result);
+        assertEquals(expectedResult, result);
+    }    
+    
+    @Test
+    public void testSort1() {
+        //Integer[] massiv = {11, 74, 125};
+        //Integer[] expectedResult = {11, 125, 74};
+        //Integer[] result = Homework9_1.sort(massiv);
+        int expectedResult = 1;
+        int result = 1;
+        //Assert.assertArrayEquals(expectedResult, result);
+        assertEquals(expectedResult, result);
+    }    
+   /*
+    @Test
     public void testSort() {
-        int[] result = hw9_1.sort(testData);
-        assertTrue(Arrays.equals(expectedResult, result));
+        Integer[] result = hw9_1.sort(testData);
+        Assert.assertArrayEquals(expectedResult, result);
     }
 
     @Parameterized.Parameters
-    public static List<int[][]> summDigitsData() {
-        return Arrays.asList(new int[][][]{
-            {{25, 1627, -2147483648, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421}, {7, 16, 47, 36, 46, 15, 1, 0, 1, 5, 12}},
-            {{1111111111, 111111111, 11111111, 1111111, 111111, 11111, 1111, 111, 11, 1}, {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}},
-            {{0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0, 0}},
+    public static List<Integer[][]> sortData() {
+        return Arrays.asList(new Integer[][][]{
+            {{25, 1627, -2147483646, 9999, 2147483647, 753, 1, 0, 10, 5000, 5421}, {0, 1, 10, 5000, 25, 5421, 753, 1627, 9999, -2147483646, 2147483647}},
+            {{1111111111, 111111111, 11111111, 1111111, 111111, 11111, 1111, 111, 11, 1}, {1, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111, 1111111111}},
+            {{0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 1}},
             {{5},{5}}
         });
     }
+    */
     
-    @Parameterized.Parameters
-    public static List<int[][]> sortData() {
-        return Arrays.asList(new int[][][]{
-            {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}},
-            {{5, 4, 3, 2, 1}, {1, 2, 3, 4, 5}},
-            {{4, 1, 2, 5, 3}, {1, 2, 3, 4, 5}},
-            {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}}
-        });
-    }
 }
